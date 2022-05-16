676b46e16a593e50a6d8b8709ed7bec5e09768bf
Cristian Tulbu <96737712+Cristian6918@users.noreply.github.com>
2022-05-16 17:06:32 +0200

    Products classes

--- product.php
+++ product.php
@@ -7,6 +7,7 @@ class Product
     public $category;
     public $price;
     public $id;
+    public $prod_for = [];
 
     function __construct($_name, $_price, $_category)
     {
@@ -19,14 +20,55 @@ class Product
     {
         $this->description = $_description;
     }
+
+    function setProduct_for($_prod_for)
+    {
+        $this->prod_for = $_prod_for;
+    }
 }
 
 class Food extends Product
 {
-    public $prod_for = [];
-};
+    public $taste_of;
+    public $weight;
 
-$mensola = new Product('Ciao', 12, 'animali');
+    function __construct($_name, $_price, $_taste_of)
+    {
+        $this->name = $_name;
+        $this->price = $_price;
+        $this->taste_of = $_taste_of;
+    }
+    function setWeight($_weight)
+    {
+        $this->weight = $_weight;
+    }
+};
+class Toy extends Product
+{
+    public $type;
+    function __construct($_name, $_price, $_type)
+    {
+        $this->name = $_name;
+        $this->price = $_price;
+        $this->type = $_type;
+    }
+}
+
+class Accessories extends Product
+{
+    public $object;
+    public $color;
+
+
+    function __construct($_name, $_price, $_object)
+    {
+        $this->name = $_name;
+        $this->price = $_price;
+        $this->type = $_object;
+    }
+}
+
+$mensola = new Toy('Ciao', 12, 'animali');
 
 echo ('<pre>');
 var_dump($mensola);
